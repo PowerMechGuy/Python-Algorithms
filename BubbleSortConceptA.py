93062df21713961eabad5fde2709198ce6bc91e2
Classified <powermechguy@gmail.com>
2019-11-08 00:13:38 -0500

    changing to completed program

--- BubbleSortConceptA.py
+++ BubbleSortConceptA.py
@@ -1,72 +1,121 @@
-userInput = input("Input a number to be sorted: ")
-num = int(userInput)
+#DEFINING FUNCTIONS
+def determineSize(inputNumber):
+	size = 0
+	counter = 1
+	sizeDetermined = False
+	while sizeDetermined == False:
+		check = inputNumber / counter
+		if check < 1:
+			sizeDetermined = True
+		else:
+			size += 1
+			counter *= 10
+	#END size determination while
+	print("There are " + str(size) + " digits in the number you entered...")
+	return counter, size, inputNumber
+#END  size determination function
 
-print("Setting up variables...")
-
-#determine how many digits there are
-sizeDetermined = False
-size = 0
-counter = 1
-
-while sizeDetermined == False:
-	check = num / counter
-	if check < 1:
-		sizeDetermined = True
-	else:
-		size += 1
-		counter *= 10
-#END size determination while
-
-print("There are " + str(size) + " digits in the number you entered...")
-
-numberList = []
-sortedNumber = []
-counter /= 10 
-
-for i in range(size):
-	digit = int(num // counter)
-	if i > 0:
+def initializeList(oldCounter, oldSize, oldInputNumber):
+	oldCounter /= 10
+	numberList = []
+	for i in range(oldSize):
+		digit = int(oldInputNumber // oldCounter)
 		digit = digit - ((int(digit // 10)) * 10)
-	numberList.append(int(digit))
-	if counter != 1:
-		counter /= 10
-#END numberList initialization for loop
+		print("Digit = " + str(digit))
+		numberList.append(int(digit))
+		if oldCounter != 1:
+			oldCounter /= 10
+		#END counter_is_not_equal_to_one_check if
+	#END numberList initialization for loop
+	return numberList
+#END list initialization function
 
-def printList():
-	for number in numberList:
+def printList(inputList):
+	for number in inputList:
 		print(number, end=' ')
 	#END list printing for loop
+	print("\n")
 #END numberList printing function
 
-printList()
-
-print("\n")	
+def sort(inputList):
+	#Number container has been inititialized and is ready for sorting.
+	print("Variable ready. Bubble Sorting now...")
+ 
+	numberList = inputList
+	sortedNumber = []
 	
-#Number container has been inititialized and is ready for sorting.
-print("Variable ready. Bubble Sorting now...")
-#################################
-
-def sort():
-	#This function will sort the number
-	#using bubble sort
+	sortingComplete = False
 	
-	sorted = False
+	while(sortingComplete == False):
 	
-	length = len(numberList)
+	    length = len(numberList)
+	    #print("Length = " + str(length))
 	
-	print("Length of numberList variable: " + str(length))
+	    spotA = length - 2
+	    spotB = length - 1
+	    
+	    if(length == 2):
+	    	a = numberList[spotA]
+	    	b = numberList[spotB]
+	    	if(a > b):
+	    		c = numberList[spotA]
+	    		numberList[spotA] = b
+	    		numberList[spotB] = c
+	    		sortedNumber.extend(numberList)
+	    		sortingComplete = True
+	    	#END final sort if
+	    	
+	    	else:
+	    		sortedNumber.extend(numberList)
+	    		sortingComplete = True
+	    	#END normal case else
+	    #END length check if
+	    		    		    	
+	    if(sortingComplete == False):
+	        for j in range(length - 1):
 	
-	a = numberList[length - 2]
-	b = numberList[length - 1]
+	            a = numberList[spotA]
+	            b = numberList[spotB]
+	    
+	            if(a > b):
+	    	        c = numberList[spotA]
+	    	        numberList[spotA] = b
+	    	        numberList[spotB] = c
+	            #END number switch if
+	    
+	            spotA -= 1
+	            spotB -= 1
+	        #END single iteration for loop
+	    #END sorted-check if
+	    
+	    print("Value after partial sort: ")
+	    printList(numberList)
+	    
+	    if(sortingComplete == False):
+	        sortedNumber.append(numberList[0])
+	        del numberList[0]
+	        print("Old List: ")
+	        printList(numberList)
+	        print("New List: ")
+	        printList(sortedNumber)
+	    #END sorted-check if
+	#END iterative while loop
 	
-	if a > b:
-		c = numberList[length - 2]
-		numberList[length - 2] = b
-		numberList[length - 1] = c
-	#END switch numbers if
-	
-	printList()
+	print("Fully Sorted Number: ")
+	printList(sortedNumber)
 	
 #END main sorting function
 
-sort()
+#PROGRAM FLOW
+userInput = input("Input a number to be sorted: ")
+
+(counterTransfer, sizeTransfer, numberTransfer) = determineSize(int(userInput))
+print("counterTransfer = " + str(counterTransfer))
+print("sizeTransfer = " + str(sizeTransfer))
+print("numberTransfer = " + str(numberTransfer))
+
+output = initializeList(counterTransfer, sizeTransfer, numberTransfer)
+print("Initialized List:", end=" ")
+printList(output)
+
+sort(initializeList(counterTransfer, sizeTransfer, numberTransfer))
